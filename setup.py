6d77e26c9b67e25d13c2a5fdd092db5d948f94f3
Till Wiese <1872304+m3adow@users.noreply.github.com>
2023-11-04 20:19:02 +0100

    Update kubernetes-validate version

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     description="A pre-commit hook to validate Kubernetes YAML files",
     url="https://github.com/Agilicus/pre-commit-hook-k8svalidate",
     version="0.1.1",
-    install_requires=["ruamel.yaml>=0.16.10", "kubernetes-validate>=1.21.0"],
+    install_requires=["ruamel.yaml>=0.16.10", "kubernetes-validate>=1.27.1"],
     scripts=["pre-commit-hooks/k8svalidate"],
 )
